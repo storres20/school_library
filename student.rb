ff85d7ad3a2bc39b4a171294471883db1ac826c5
Italo Lon kan <sanchotorres777@gmail.com>
2023-01-30 19:50:54 -0500

    Fix linter's error message

--- student.rb
+++ student.rb
@@ -2,8 +2,9 @@ require './person'
 
 class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission:)
+    super(age, name, parent_permission: true)
     @classroom = classroom
+    @parent_permission = parent_permission
   end
 
   def play_hooky
--- teacher.rb
+++ teacher.rb
@@ -2,8 +2,9 @@ require './person'
 
 class Teacher < Person
   def initialize(specialization, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission:)
+    super(age, name, parent_permission: true)
     @specialization = specialization
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
